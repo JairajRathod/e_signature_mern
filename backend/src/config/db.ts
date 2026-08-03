3997bbfad98f3fcd9bfcb8eac35a6ac238de8c8e
jairaj rathod <rathodjairaj805@gmail.com>
2026-08-04 01:19:57 +0530

    mongodb connection string update

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -8,14 +8,16 @@ export const dbConnect = async (): Promise<void> => {
       throw new Error("MONGO_URI is not defined.");
     }
 
-    await mongoose.connect(mongoUri);
+    const conn = await mongoose.connect(mongoUri);
 
     console.log("MongoDB Connected Successfully");
+    console.log("Host:", conn.connection.host);
+    console.log("Database Name:", conn.connection.name);
+    console.log("Connection Port:", conn.connection.port);
+    console.log("Connection Ready State:", conn.connection.readyState);
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
-
     console.error("MongoDB Connection Error:", message);
-
     throw new Error(`Database connection failed: ${message}`);
   }
 };
